9fbe2c0c2abe8e152cf76c31545abdf0834e7527
pongtsu <pongtsu@bitbucket.com>
2016-03-12 14:51:36 +0700

    Creat multiple function

--- calculator.js
+++ calculator.js
@@ -7,3 +7,8 @@ function sub(x, y){
   if(isNaN(x)) return console.log('Input must be number.');
   return x - y;
 }
+
+function mul(x, y){
+  if(isNaN(x)) return console.log('Input must be number.');
+  return x * y;
+}
